286dfd04aa85db98502227c49277396e4ec0180b
skaletin231 <jacobskaggs03@yahoo.com>
2026-09-11 12:18:18 -0400

    added game loss screen now too

--- Frontend/src/Components/Popups/GameOverDialogue.jsx
+++ Frontend/src/Components/Popups/GameOverDialogue.jsx
@@ -12,34 +12,47 @@ import CloseIcon from '@mui/icons-material/Close'
 import { START_GAME, ME } from '../../queries'
 import { useMutation } from '@apollo/client/react'
 
-const imageStyle = {
-  height: '130%',
-  alignSelf: 'end',
-}
-
 const GameOverDataSX = {
   color: '#3A1605',
   fontSize: '1rem',
   textAlign: 'center',
-}
-
-const GameOverHeaderSX = {
-  fontSize: '3.5rem',
-  fontFamily: '"Luckiest Guy", serif',
-  color: '#286B1F',
-  textAlign: 'center',
   lineHeight: '1',
 }
 
-const GameOverMessageSX = {
-  color: '#1E5C15',
-  fontSize: '1.25rem',
-  fontWeight: 'bold',
-  textAlign: 'center',
+const gameStates = {
+  hint: 'Hint',
+  win: 'Win',
+  lose: 'Lose',
+  playing: 'Playing',
+  waiting: 'waiting',
 }
 
 function GameOverScreen({ open, setOpen, game }) {
   const navigate = useNavigate()
+  const gameWon = game.gameState === gameStates.win
+
+  const imageStyle = {
+    height: '130%',
+    alignSelf: 'end',
+    padding: gameWon ? '20px 0px' : '0px',
+  }
+
+  const GameOverHeaderSX = {
+    fontSize: '3.5rem',
+    fontFamily: '"Luckiest Guy", serif',
+    color: gameWon ? '#286B1F' : '#B43131',
+    textAlign: 'center',
+    //marginBlock: '-12px',
+    lineHeight: '1',
+  }
+
+  const GameOverMessageSX = {
+    color: gameWon ? '#1E5C15' : '#B43131',
+    fontSize: '1.25rem',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    lineHeight: '1',
+  }
 
   const [startGame] = useMutation(START_GAME, {
     refetchQueries: [ME],
@@ -72,66 +85,43 @@ function GameOverScreen({ open, setOpen, game }) {
     navigate('/')
   }
 
-  const gameWin = () => {
+  const gameWinText = () => {
     return (
       <>
-        <Box
-          className='flexRow'
-          sx={{
-            gap: '30px',
-            height: '260px',
-            width: '700px',
-            flexShrink: '1',
-          }}
-        >
-          <img src={gameWonImage} style={imageStyle} />
-
-          <Box className='flexColumn' sx={{ justifyContent: 'space-evenly' }}>
-            <Typography sx={GameOverHeaderSX}>YOU WIN!</Typography>
-            <Typography sx={GameOverDataSX}>
-              {game.maxTurns - game.turnsRemaining}{' '}
-              {game.maxTurns - game.turnsRemaining === 1 ? 'round' : 'rounds'} •{' '}
-              {game.mistakes} {game.mistakes === 1 ? 'mistake' : 'mistakes'}{' '}
-              {game.mistakeLimit !== -1 && (
-                <>
-                  • {game.mistakes}/{game.mistakeLimit} mistakes made
-                </>
-              )}
-            </Typography>
-            <Typography sx={GameOverMessageSX}>
-              Thanks to you, no one had to blow up!
-            </Typography>
-            <Box
-              className='flexRow'
-              sx={{
-                gap: '10px',
-                justifyContent: 'center',
-                isolation: 'isolate',
-              }}
-            >
-              <Button
-                className='buttonStyle3D'
-                variant='contained'
-                onClick={handleSubmit}
-              >
-                Play Again
-              </Button>
-              <Button
-                className='brown buttonStyle3D'
-                variant='contained'
-                onClick={handleReturnHome}
-              >
-                Exit
-              </Button>
-            </Box>
-          </Box>
-        </Box>
+        <Typography sx={GameOverHeaderSX}>YOU WIN!</Typography>
+        <Typography sx={GameOverDataSX}>
+          {game.maxTurns - game.turnsRemaining}{' '}
+          {game.maxTurns - game.turnsRemaining === 1 ? 'round' : 'rounds'} •{' '}
+          {game.mistakes} {game.mistakes === 1 ? 'mistake' : 'mistakes'}{' '}
+          {game.mistakeLimit !== -1 && (
+            <>
+              • {game.mistakes}/{game.mistakeLimit} mistakes made
+            </>
+          )}
+        </Typography>
+        <Typography sx={GameOverMessageSX}>
+          Thanks to you, no one had to blow up!
+        </Typography>
       </>
     )
   }
 
-  const gameLose = () => {
-    return <p>You Lose</p>
+  const gameLossText = () => {
+    return (
+      <>
+        <Typography sx={GameOverHeaderSX}>GAME OVER...</Typography>
+        <Typography sx={GameOverMessageSX}>{getGameLossText()}</Typography>
+      </>
+    )
+  }
+
+  const getGameLossText = () => {
+    if (game.mistakeLimit >= 0 && game.mistakes > game.mistakeLimit)
+      return 'You made too many mistakes!'
+    if (game.board.spots.some((spot) => spot.typeRevealed.myType === 'bomb'))
+      return 'You activated a bomb!'
+
+    return 'you ran out of time!'
   }
 
   return (
@@ -146,7 +136,7 @@ function GameOverScreen({ open, setOpen, game }) {
           sx: {
             borderRadius: '40px',
             overflow: 'visible',
-            backgroundColor: '#BBE890',
+            backgroundColor: gameWon ? '#BBE890' : '#FFB7B7',
             border: '.15rem solid white',
           },
         },
@@ -167,9 +157,55 @@ function GameOverScreen({ open, setOpen, game }) {
           }}
         />
       </IconButton>
-      <DialogContent className='DialogueTest' sx={{ overflow: 'visible' }}>
-        {game.gameState === 'Win' && gameWin()}
-        {game.gameState === 'Lose' && gameLose()}
+      <DialogContent
+        className='DialogueTest'
+        sx={{ overflow: 'visible', padding: '0px 24px' }}
+      >
+        <Box
+          className='flexRow'
+          sx={{
+            gap: '30px',
+            height: '300px',
+            width: '720px',
+            flexShrink: '1',
+          }}
+        >
+          {gameWon ? (
+            <img src={gameWonImage} style={imageStyle} />
+          ) : (
+            <img src={gameLostImage} style={imageStyle} />
+          )}
+          <Box
+            className='flexColumn'
+            sx={{ justifyContent: 'space-evenly', padding: '30px 0px' }}
+          >
+            {gameWon ? <>{gameWinText()}</> : <>{gameLossText()}</>}
+            <Box
+              className='flexRow'
+              sx={{
+                gap: '10px',
+                justifyContent: 'center',
+                isolation: 'isolate',
+                marginTop: '10px',
+              }}
+            >
+              <Button
+                className='buttonStyle3D'
+                variant='contained'
+                onClick={handleSubmit}
+              >
+                Play Again
+              </Button>
+              <Button
+                className='brown buttonStyle3D'
+                variant='contained'
+                onClick={handleReturnHome}
+              >
+                Exit
+              </Button>
+            </Box>
+          </Box>
+        </Box>
       </DialogContent>
     </Dialog>
   )
